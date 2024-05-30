48fa909718202b15acb7775608a427a47f99b8d0
Fernando Zaldivar <161621836+ferzaldivar@users.noreply.github.com>
2024-05-29 22:30:54 -0300

    Update Instalar_firefox_paso2.sh

--- Instalar_firefox_paso2.sh
+++ Instalar_firefox_paso2.sh
@@ -5,4 +5,5 @@
 
 gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
 
-echo 'Continuar con el tercer instalador'
+echo '...continua con el tercer instalador'
+sudo ./Instalar_firefox_paso3.sh
