e9480884e870179ffac6e2f474b9c34d537ecda2
Fernando Zaldivar <161621836+ferzaldivar@users.noreply.github.com>
2024-05-29 22:39:37 -0300

    Update and rename Instalar_firefox_paso2.sh to paso2.sh

--- paso2.sh
+++ paso2.sh
@@ -5,5 +5,4 @@
 
 gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
 
-echo '...continua con el tercer instalador'
-sudo ./Instalar_firefox_paso3.sh
+sudo ./paso3.sh
