0ce2ca03c5022d2eba8f28e97d03412203a25031
Fernando Zaldivar <161621836+ferzaldivar@users.noreply.github.com>
2024-05-29 22:16:20 -0300

    Add files via upload

---
+++ Instalar_firefox_paso4.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# Último paso
+# Actualiza los paquetes e instala Firefox y su paquete en español:
+
+sudo apt-get update && sudo apt-get install firefox
+sudo apt-get install firefox-l10n-es-ar
