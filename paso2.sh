1b9317da8ac7ed09038b696ba3c2cba58629c802
Fernando Zaldivar <161621836+ferzaldivar@users.noreply.github.com>
2024-06-05 23:38:00 -0300

    Delete paso2.sh

--- paso2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Segundo paso en la instalación de Firefox
-# 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3. El siguiente comando es para verificar la clave:
-
-gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
-
-sudo ./paso3.sh
