b83e3ff45b2b333ca5ee696cc6a445381a4a5bc8
Fernando Zaldivar <161621836+ferzaldivar@users.noreply.github.com>
2024-06-05 23:41:33 -0300

    Add files via upload

---
+++ paso1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Importa la clave del repositorio APT de Firefox:
+
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+bash paso2.sh
+
+
+
+
+
