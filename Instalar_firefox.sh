4b2a8246febb36335473c3b6f539259aa15b4f2e
Fernando Zaldivar <161621836+ferzaldivar@users.noreply.github.com>
2024-06-05 23:42:28 -0300

    Update Instalar_firefox.sh

--- Instalar_firefox.sh
+++ Instalar_firefox.sh
@@ -5,10 +5,6 @@
 
 sudo install -d -m 0755 /etc/apt/keyrings
 
-# Importa la clave del repositorio APT de Firefox:
-
-#wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-
 bash paso1.sh
 
 
